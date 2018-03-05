3cd7ab8e378851d421fab0295bd7091c787d8cdd
Prasanta Behera <prasanta.behera@xcubelabs.com>
2018-03-05 18:06:51 +0530

    implemented Seacrh Functionalities

--- roadnote46/src/pages/fines-and-penalties/fines-and-penalties.ts
+++ roadnote46/src/pages/fines-and-penalties/fines-and-penalties.ts
@@ -17,28 +17,55 @@ import { violation_fines } from '../../models/finesAndPenalties/filesAndPenaltie
   selector: 'page-fines-and-penalties',
   templateUrl: 'fines-and-penalties.html',
 })
+//use a loader at the time of api call
 export class FinesAndPenaltiesPage {
+  private showPenalties: Array<violation_fines>;
+  private savedPenalties: Array<violation_fines>;
+  private searchQuery: string;
 
-  finesPenalties$ : Observable<violation_fines[]>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, app: App, menu: MenuController, 
+  constructor(public navCtrl: NavController, public navParams: NavParams, app: App, menu: MenuController,
     private fineAndPenalties: FinesAndPenaltiesService) {
 
-  	menu.enable(true);
-  	menu.swipeEnable(true);
-    
-    this.finesPenalties$ = this.fineAndPenalties
-    .getFinesAndPenalties() // DB List
-    .snapshotChanges() // Key and Value
-    .map(changes => {
-        return changes.map(c => ({
-          key: c.payload.key, ...c.payload.val()
-        }));
+    menu.enable(true);
+    menu.swipeEnable(true);
+
+    // this.finesPenalties$ = this.fineAndPenalties
+    //   .getFinesAndPenalties() // DB List
+    //   .snapshotChanges() // Key and Value
+    //   .map(changes => {
+
+    //     return changes.map(c => ({
+    //       key: c.payload.key, ...c.payload.val()
+    //     }));
+    //   });
+
+    let _this = this;
+    this.fineAndPenalties.getFinesAndPenalties().snapshotChanges().subscribe(snapshots => {
+      let penalities = [];
+      snapshots.forEach(snapshot => {
+        penalities.push(snapshot.payload.val());
       });
+      _this.showPenalties = penalities;
+      _this.savedPenalties = penalities;
+    })
+
   }
+
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FinesAndPenaltiesPage');
   }
-
+  searchPenalties() {
+    if (this.searchQuery !== '' || this.searchQuery === undefined) {
+      let searchContent = []
+      this.savedPenalties.forEach((penality) => {
+        if (penality.vf_name.indexOf(this.searchQuery) != -1) {
+          searchContent.push(penality)
+        }
+      })
+      this.showPenalties = searchContent
+    }
+    else {
+      this.showPenalties = this.savedPenalties
+    }
+  }
 }
